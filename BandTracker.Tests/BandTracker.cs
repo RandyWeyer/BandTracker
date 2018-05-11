292e05c7c9889e65b3679858d6adb91232236946
UnsungGhost <randypweyer@gmail.com>
2018-05-11 09:19:36 -0700

    Initial Commit

---
+++ BandTracker.Tests/BandTracker.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BandTracker.Models;
+using System;
+
+namespace BandTracker.Tests
+{
+    [TestClass]
+    public class BandTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Band.DeleteAll();
+        }
+        public BandTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=;port=3306;database=band_tracker;";
+        }
+
+        [TestMethod]
+        public void BandList_isEmpty()
+        {
+            int result = Band.GetAll().Count;
+
+            Assert.AreEqual(0, result);
+        }
+    }
+
+    [TestClass]
+    public class VenueTests
+    {
+        public VenueTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=;port=3306;database=band_tracker;";
+        }
+
+    }
+}
